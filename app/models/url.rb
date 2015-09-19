bf835cf80ba64a3085cb04e4eeef0a03306b9fb3
Maxim Djuliy <mak7.dj@gmail.com>
2015-09-19 11:46:11 +0300

    add #crypt_data for generationg short url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,16 @@
 # URL model
 class Url < ActiveRecord::Base
+
+  private
+
+  def crypt_data(number)
+    out   = ''
+    codes = 'rstbuABC890cPQRSTUVWdaefghlmnpq567DEFGHJKLMNOXYZ234xyzijk'
+    while number > 56
+      key    = number % 57
+      number = (number / 57).floor - 1
+      out    = "#{codes[key]}#{out}"
+    end
+    "#{codes[number]}#{out}"
+  end
 end
